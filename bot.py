7dbced9efc73cc06a5c249ebc5e1a249b20d7167
Harsha Lakamsani <hlakamsani@gmail.com>
2020-09-11 18:09:10 -0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -19,4 +19,41 @@ async def buzz(ctx):
 
     await ctx.send("{} buzzed at {}".format(ctx.message.author.mention, current)) # print buzz
 
+
+global countOne # var for room #1 counter
+
+@bot.command(name='one') # question count for room #1
+async def one(ctx):
+
+    global countOne
+    countOne += 1
+    await ctx.send("Room 1\n----------\nQuestion {}\n----------\n".format(countOne))
+
+
+@bot.command(name='oneC') # reinitialize countOne
+async def oneC(ctx):
+
+    global countOne
+    countOne = 0
+    await ctx.send("*Round for Room #1 has been Reset*")
+
+
+global countTwo # var for room #2 counter
+
+@bot.command(name='two') # question count for room #2
+async def two(ctx):
+
+    global countTwo
+    countTwo += 1
+    await ctx.send("Room 2\n----------\nQuestion {}\n----------\n".format(countTwo))
+
+
+@bot.command(name='twoC') # reinitialize countTwo
+async def twoC(ctx):
+
+    global countTwo
+    countTwo = 0
+    await ctx.send("*Round for Room #2 has been Reset*")
+
+
 bot.run(TOKEN) # run bot
